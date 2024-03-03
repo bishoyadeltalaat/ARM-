7758b0fc2b97ccc2b6c7aa902440bcf176893103
bishoy <bishoyadel1@gmail.com>
2024-03-03 09:51:42 +0200

    RCC

--- MCAL/RCC/RCC_program.c
+++ MCAL/RCC/RCC_program.c
@@ -1,3 +1,15 @@
+/*********************************************
+ * Author:				Bishoy Adel
+ * Creation Data:		1 Mar, 2024
+ * Version:				v1.0
+ * Compiler:			GNU ARM-GCC
+ * Controller:			STM32F401CCU6
+ * Layer:				MCAL
+ ********************************************/
+/*********************************************
+ * Version	  Date				  Author				  Description
+ * v1.1		  3 Mar, 2024		Bishoy Adel			    Initial Creation
+*********************************************/
 //Library Inclusion
 #include "../include/STD_TYPES.h"
 #include "../include/BIT_MATH.h"
